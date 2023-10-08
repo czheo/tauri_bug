01235a50278cec60102fd8bd243ab0cea1dc91d9
czheo <1692400+czheo@users.noreply.github.com>
2023-10-07 22:46:13 -0700

    original template

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri::CustomMenuItem;
-use tauri::Menu;
-
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,9 +8,7 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    let menu = Menu::new().add_item(CustomMenuItem::new("hide", "Hide"));
     tauri::Builder::default()
-        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
